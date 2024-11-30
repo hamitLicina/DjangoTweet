b0f64c6b62ba999ff90aa66b41d8701fa17b2317
hamitLicina <hamitlicina@gmail.com>
2024-11-30 23:13:36 +0300

    Update tweet addition functionality and view logic
    - Refined ddtweet.html template for improved tweet creation interface.
    - Adjusted logic in 	weetapp/views.py to fix issues and enhance functionality.
    - Updated compiled view cache in __pycache__.
    
    These changes improve the user experience and optimize backend processes for tweet management.

--- djangotweet/tweetapp/views.py
+++ djangotweet/tweetapp/views.py
@@ -9,6 +9,11 @@ def listtweet(request):
     return render(request, 'tweetapp/listtweet.html', context=tweet_dict)
 
 def addtweet(request):
+    if request.POST:
+        nickname = request.POST["nickname"]
+        message = request.POST["message"]
+        new_tweet = models.Tweet(nickname=nickname, message=message)
+        new_tweet.save()
     return render(request, 'tweetapp/addtweet.html')
 
 def profile(request):
