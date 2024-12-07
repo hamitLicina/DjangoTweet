2d3b1197c2e424e38604bb17b0d6a42ec86a8f14
hamitLicina <hamitlicina@gmail.com>
2024-12-07 17:55:25 +0300

    Updated settings, URLs, and base template; added registration templates to enhance authentication functionality

--- djangotweet/djangotweet/settings.py
+++ djangotweet/djangotweet/settings.py
@@ -123,3 +123,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+
+LOGOUT_URL = '/logout'
+LOGOUT_REDIRECT_URL = '/'
